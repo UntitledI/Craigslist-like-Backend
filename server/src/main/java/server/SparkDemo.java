9f4a52e96eab67e8eab23957972dd6ced524fc35
dimitri <drodriguez@mail.sfsu.edu>
2021-05-14 16:59:50 -0700

    server

--- server/src/main/java/server/SparkDemo.java
+++ server/src/main/java/server/SparkDemo.java
@@ -1,27 +1,46 @@
 package server;
 
 import com.google.gson.Gson;
+import dao.ListingDao;
+import dto.ListingDto;
+import dto.ResponseDto;
+import processor.AddListingProcessor;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static spark.Spark.*;
 
 public class SparkDemo {
+
+  private static Gson gson = new Gson();
+
   public static void main(String[] args) {
     port(1235);
     webSocket("/ws", WebSocketHandler.class);
     post("/api/createListing", (request, response) -> {
       String body = request.body();
-
-      return null;
+      List<ListingDto> addList= new ArrayList<>();
+      ListingDto item = gson.fromJson(body,ListingDto.class);
+      ListingDto hi = ListingDao.getInstance().put(item);
+      addList.add(hi);
+      ResponseDto addItem = new ResponseDto(new Date(),addList, true);
+      return addItem;
     });
 
     delete("/api/deleteListing", (request, response) -> {
+
       return null;
     });
 
     get("api/viewListings", (request, response) -> {
+
+      return null;
+    });
+
+    get("/api/filterListings", (request, response) -> {
+
       return null;
     });
 
