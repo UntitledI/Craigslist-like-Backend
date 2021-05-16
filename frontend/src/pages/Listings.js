4057b17d04c88d0ca95469fe300c3ed5760ad542
Lean278 <lcampana@mail.sfsu.edu>
2021-05-15 22:26:52 -0700

    started main frontend layout

--- frontend/src/pages/Listings.js
+++ frontend/src/pages/Listings.js
@@ -1,12 +1,48 @@
 //View Listings
 //Must link addListings to this one
 
+import react from 'react'
+import axios from 'axios'
+
+    const [description, setDescription] = React.useState('');
+    const [type, setType] = React.useState('');
+    const [price, setPrice] = React.useState('');
+    const [title, setTitle] = React.useState('');
+    const [email, setEmail] = React.useState('');
+
+    const listing = [
+        description, type, price, title, email
+    ];
+
+//Command to fetch list (IMPORTANT)
+    const fetchListings = () => {
+        axios.get('/api/viewListings');
+    };
+
+//Command to delete listing from the list
+    const handleRemove = (title) => {
+    axios.get('/api/deleteListing')
+        .then((res) => {
+            console.log(res.data);
+            const newList = list.filter((item) => item.title !== title);
+            setListingsList(newList);
+        });
+    };
+
 const listings = () => {
     return (
     <div>
         <h1><link to="Home">Home</link></h1>
         <h1><link to="Chat">Chat</link></h1>
         <h1><link to="Listings">Site Listings</link></h1>
+
+        <h1>Current Listings</h1>
+        <div>
+        {/* Display list here */
+          listing.map((object, i) => <div key={i}>{object.list}
+          <button onClick={() => handleRemove(item.title)}></button>
+        </div>)}; 
+        </div>
     </div>
     );
 }
--- frontend/src/pages/addListings.js
+++ frontend/src/pages/addListings.js
@@ -2,12 +2,81 @@
 import React from 'react';
 import axios from 'axios';
 
+//For input
+const [description, setDescription] = React.useState('');
+const [type, setType] = React.useState('');
+const [price, setPrice] = React.useState('');
+const [title, setTitle] = React.useState('');
+const [email, setEmail] = React.useState('');
+
+const listing = [
+    description, type, price, title, email
+];
+
+//Command to submit listing
+const handleSubmit = () => {
+    console.log(description, type, price, title);
+    const body = {
+      description, type, price, title
+    };
+    axios.post('/api/createListing', body)
+      .then(fetchListings);
+  };
+
+  //Commands to update listings
+  const handleDescription= (e) => {
+    setDescription(e.target.value);
+  };
+  const handleType = (e) => {
+    setType(e.target.value);
+  };
+  const handlePrice = (e) => {
+    setPrice(e.target.value);
+  };
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  //TODO: Link addListings to the main listings page
+
 const addListings = () => {
     //note: it would be more logical to view listings before adding one
     <div>
         <h1><link to="Home">Home</link></h1>
         <h1><link to="Chat">Chat</link></h1>
         <h1><link to="Listings">Site Listings</link></h1>
+        <h1>Create Listing</h1>
+        <div className="Create Listing"> {
+            //Redirect to view listings page once 
+            <form action=".../Listings">
+            <label>
+                Email:
+                <input onChange={handleEmail} id="input-email" value={email} />
+            </label>
+            <label>
+                Title:
+                <input onChange={handleTitle} id="input-title" value={title} />
+            </label>
+            <label>
+                Type:
+                <input onChange={handleType} id="input-type" value={type}/>
+            </label>
+            <label>
+                Description:
+                <input onChange={handleDescription} id="input-description" value={description}/>
+            </label>
+            <label>
+                Price:
+                <input onChange={handlePrice} id="input-price" value={price}/>
+            </label>
+            {/*Submit -> handleSubmit*/}
+            <button id="submit" onClick={handleSubmit()}>Submit</button>
+          </form>
+        }
+        </div>
     </div>
 }
 
